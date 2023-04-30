195aa4356f5eb00f896e1e6a980cc972d00991f9
황현태 <47472389+Onuel-hyeon@users.noreply.github.com>
2023-04-30 18:55:03 +0900

    암호 해독

---
+++ Coding-Masters/6861.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+password = input().rstrip()
+# 1. 현재 인덱스 값, i값 설정
+now_index = 0
+i = 0
+ans = ''
+while now_index < len(password) :
+    # 3. 암호화한 문자열의 특정 인덱스만 뽑기
+    ans += password[now_index]
+    i += 1
+    now_index += i
+# 4. 결과 출력
+print(ans)
